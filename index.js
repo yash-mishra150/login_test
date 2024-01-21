65a1092de1ba11d3e5799a30485953b97a124590
Yash Mishra <praisedboy5000@gmail.com>
2024-01-21 21:48:04 +0530

    correction

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { createPool } = require("mysql");
 
 const pool = createPool({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "",
   database: "Hospital",
